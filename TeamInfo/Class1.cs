e9c607b65276957df303ae89af91797aa590f506
KCC <KCC@DESKTOP-6K2NS8V>
2024-07-04 15:11:18 -0700

    Add project files.

---
+++ TeamInfo/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace EvertonProject.Models
+{
+    public class TeamInfo
+    {
+        public string TeamName { get; set; }
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Losses { get; set; }
+        public List<Player> Players { get; set; }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Number { get; set; }
+    }
+}
